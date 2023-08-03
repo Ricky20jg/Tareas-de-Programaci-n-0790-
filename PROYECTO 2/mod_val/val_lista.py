2fea917a6bf3f3abe0137df7d63b36a1f4a85e4e
Ricardo González García <ricardo20jgg@gmail.com>
2023-08-03 08:39:01 -0400

    Update val_lista.py

--- PROYECTO 2/mod_val/val_lista.py
+++ PROYECTO 2/mod_val/val_lista.py
@@ -1,22 +1,22 @@
-def val_lista(ls, length=None, option=None):
+def val_lista(ls, long=None, opcion=None):
     # Comprobamos si el argumento es una lista
     if str(type(ls)) == list:
         # Si no se proporcionan ni longitud ni opción, simplemente devolvemos True
-        if length is None and option is None:
+        if long is None and opcion is None:
             return True
         # Sise proporciona una opción, debemos verificarla
-        elif option is not None:
+        elif opcion is not None:
             # Si la opción es "len", comprobamos si la longitud de la lista es igual a la longitud proporcionada
-            if option == "len":
-                if len(ls) == length:
+            if opcion == "len":
+                if len(ls) == long:
                     return True
                 else:
                     return False
             # Si la opción es "value", comprobamos si la lista es igual a la lista proporcionada
-            elif option == "value":
+            elif opcion == "value":
                 # Comprobamos si la longitud es una lista
-                if str(type(length)) == list:
-                    if ls == length:
+                if str(type(long)) == list:
+                    if ls == long:
                         return True
                     else:
                         return False
@@ -26,7 +26,7 @@ def val_lista(ls, length=None, option=None):
             else:
                 raise ValueError("La opción debe ser 'len' o 'value'")
         # Si se proporciona una longitud pero no una opción, simplemente devolvemos True
-        elif length is not None:
+        elif long is not None:
             return True
     else:
         return False
